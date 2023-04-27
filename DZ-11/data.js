04f257e0b357baf5200b3598acbc3408c993fb92
Calupator <calupator@mail.ru>
2023-04-27 19:09:46 +0300

    DZ-11

---
+++ DZ-11/data.js
@@ -0,0 +1,56 @@
+export const dataProducts = `[
+	{
+	"name": "ELLERY X M'O CAPSULE",
+	"image": "img/1-Image.png",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"price": 50,
+	"color": "Red",
+	"size": "L",
+	"quantity": 1
+},
+{
+	"name": "ELLERY X M'O CAPSULE",
+	"image": "img/2-Image.png",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"price": 60,
+	"color": "Green",
+	"size": "Xl",
+	"quantity": 1
+},
+{
+	"name": "MANGO PEOPLE T-SHIRT",
+	"image": "img/3-Image.png",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"price": 70,
+	"color": "Gray",
+	"size": "M",
+	"quantity": 1
+},
+{
+	"name": "ELLERY X M'O CAPSULE",
+	"image": "img/4-Image.png",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"price": 90,
+	"color": "Blue",
+	"size": "XXXL",
+	"quantity": 1
+},
+{
+	"name": "ELLERY X M'O CAPSULE",
+	"image": "img/5-Image.png",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"price": 105,
+	"color": "White",
+	"size": "Xl",
+	"quantity": 1
+},
+{
+	"name": "MANGO PEOPLE T-SHIRT",
+	"image": "img/6-Image.png",
+	"description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+	"price": 300,
+	"color": "Black",
+	"size": "XXL",
+	"quantity": 1
+}
+]`;
